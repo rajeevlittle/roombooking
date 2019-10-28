06f4a3cb372fb7d151449c062f24c94c770ddb57
ecomdevsolutions.gmail.com <ecomdevsolutions.gmail.com>
2019-10-29 04:49:10 +0900

    users get api complete

--- src/main/java/com/roombooking/demo/controller/UserController.java
+++ src/main/java/com/roombooking/demo/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.roombooking.demo.controller;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.validation.Valid;
 
 import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,11 +23,13 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/greeting")
-    public UserModel greeting(@Valid @RequestBody UserModel user) {
-//    	return body;
-//    	return user;
+    @RequestMapping("/users")
+    public UserModel createUser(@Valid @RequestBody UserModel user) {
     	return userRepository.save(user);
-//    	return body.getName();
+    }
+    
+    @GetMapping("/users")
+    public List < UserModel > getAllUsers() {
+        return userRepository.findAll();
     }
 }
